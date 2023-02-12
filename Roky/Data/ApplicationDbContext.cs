2b1925834300d3ebdb19f512c0664757cf96eeaa
Илья Емец <67930111+Dzirt089@users.noreply.github.com>
2023-02-12 17:53:13 +0300

    Добавьте файлы проекта.

---
+++ Roky/Data/ApplicationDbContext.cs
@@ -0,0 +1,6 @@
+﻿namespace Roky.Data
+{
+    public class ApplicationDbContext
+    {
+    }
+}
---
+++ Roky/Models/Category.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Roky.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+        public int DisplayOrder { get; set; }
+
+
+            
+    }
+}
